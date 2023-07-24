13dd701907333a17fbfdedf0ac108490760519ed
Umbrases <zhenua27@gmail.com>
2023-07-24 14:04:23 +0300

    fifth

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\Login\DestroyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\PasswordConfirmation\CreateController;
+use App\Http\Controllers\Auth\PasswordConfirmation\StoreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,80 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::view('/', 'welcome')->name('welcome');
+
+Route::group(['namespace' => 'Auth'] ,function (){
+    Route::group(['namespace' => 'Register', 'middleware' => 'guest'], function () {
+        Route::get('/register', 'CreateController')->name('register');
+        Route::post('/register', 'StoreController')->name('register.store');
+    });
+
+    Route::group(['namespace' => 'Login', 'middleware' => 'guest'], function () {
+        Route::get('/login', 'CreateController')->name('login');
+        Route::post('/login', 'StoreController')->name('login.store');
+    });
+
+    Route::post('/logout', DestroyController::class)->name('logout')->middleware('auth');
+
+    Route::group(['middleware' => 'auth', 'namespace' => 'EmailVerification'], function () {
+        Route::get('/email/verify', 'EmailVerificationPromptController')->name('verification.notice');
+        Route::get('/email/verify/{id}/{hash}', 'VerifyEmailController')->name('verification.verify')->middleware('signed');
+        Route::post('/email/verification-notification', 'EmailVerificationNotificationController')->name('verification.send');
+    });
+
+    Route::group(['namespace' => 'ForgotPassword', 'middleware' => 'guest'], function () {
+        Route::get('/forgot-password', 'CreateController')->name('password.request');
+        Route::post('/forgot-password', 'StoreController')->name('password.email');
+    });
+
+    Route::group(['namespace' => 'ResetPassword', 'middleware' => 'guest'], function () {
+        Route::get('/reset-password', 'CreateController')->name('password.reset');
+        Route::post('/reset-password', 'StoreController')->name('password.update');
+    });
+
+    Route::view('/profile', 'profile')->middleware(['verified', 'password.confirm'])->name('profile');
+
+    Route::get('/confirm-password', CreateController::class)->middleware('auth')->name('password.confirm');
+    Route::post('/confirm-password', StoreController::class)->middleware('auth');
+
+    Route::group(['namespace' => 'TwoFA'], function (){
+        Route::get('/dashboard', 'IndexController')->middleware('verified')->name('dashboard');
+        Route::post('/2fa', 'StoreController')->name('2fa.post');
+        Route::get('2fa/reset', 'ResendController')->name('2fa.resend');
+    });
+});
+
+
+Route::get('/search', 'SearchController')->name('search.index');
+
+
+Route::group(['namespace' => 'LivingSpace', 'middleware' => '2fa'], function (){
+    Route::get('/livingSpace/create', 'CreateController')->name('create');
+    Route::post('/livingSpace', 'StoreController')->name('store');
+});
+
+
+Route::group(['namespace' => 'Flat'], function (){
+    Route::get('/flat', 'IndexController')->name('flat.index');
+});
+
+
+Route::group(['namespace' => 'Room'], function (){
+    Route::get('/room', 'IndexController')->name('room.index');
+});
+
+
+Route::group(['namespace' => 'House'], function (){
+    Route::get('/house', 'IndexController')->name('house.index');
+});
+
+
+Route::group(['namespace' => 'Sale'], function (){
+    Route::get('/sale/{livingSpace}', 'ShowController')->name('sale.show');
+    Route::get('/sale/{livingSpace}/edit', 'EditController')->name('sale.edit');
+    Route::patch('/sale/{livingSpace}', 'UpdateController')->name('sale.update');
+    Route::delete('/sale/{livingSpace}', 'DestroyController')->name('sale.destroy');
+
 });
